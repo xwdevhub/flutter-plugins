9792971e2c32c0a513c1a20f615572b69f41cd69
songwenchuang <1572160921@qq.com>
2023-06-09 17:05:17 +0800

    bugfix：修复路径编码

--- packages/pasteboard/windows/pasteboard_plugin.cpp
+++ packages/pasteboard/windows/pasteboard_plugin.cpp
@@ -299,6 +299,22 @@ namespace
 		hdr->biClrImportant = 0;
 	}
 
+	string Utf8ToGbk(const char* src_str)
+	{
+		int len = MultiByteToWideChar(CP_UTF8, 0, src_str, -1, NULL, 0);
+		wchar_t* wszGBK = new wchar_t[len + 1];
+		memset(wszGBK, 0, len * 2 + 2);
+		MultiByteToWideChar(CP_UTF8, 0, src_str, -1, wszGBK, len);
+		len = WideCharToMultiByte(CP_ACP, 0, wszGBK, -1, NULL, 0, NULL, NULL);
+		char* szGBK = new char[len + 1];
+		memset(szGBK, 0, len + 1);
+		WideCharToMultiByte(CP_ACP, 0, wszGBK, -1, szGBK, len, NULL, NULL);
+		string strTemp(szGBK);
+		if (wszGBK) delete[] wszGBK;
+		if (szGBK) delete[] szGBK;
+		return strTemp;
+	}
+
 	std::wstring stringToWstring(const std::string &str)
 	{
 		LPCSTR pszSrc = str.c_str();
@@ -636,6 +652,7 @@ namespace
 			}
 
 			string str = std::string(data1.data(), data1.data() + data1.size());
+			str=Utf8ToGbk(str.c_str());
 			std::cout << str << std::endl;
 			wstring ss = stringToWstring(str);
 			Gdiplus::Bitmap *image1 = Gdiplus::Bitmap::FromFile(ss.c_str());
